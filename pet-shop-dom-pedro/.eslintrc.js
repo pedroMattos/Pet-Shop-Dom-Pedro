6a1c531737675a63cdd01874cad178036e75a5df
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-03-30 20:18:53 -0300

    add project structure

---
+++ pet-shop-dom-pedro/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true,
+  },
+
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+  ],
+
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off',
+  },
+
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/airbnb'
+  ]
+};
