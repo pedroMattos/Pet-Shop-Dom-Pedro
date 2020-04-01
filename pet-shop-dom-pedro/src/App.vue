3ddb4bbc00edbd9c1c45af90dbd2a91043ea61ab
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-04-01 20:05:08 -0300

    include sidebar

--- pet-shop-dom-pedro/src/App.vue
+++ pet-shop-dom-pedro/src/App.vue
@@ -1,8 +1,30 @@
 <template>
   <div id="app">
-    <router-view/>
+    <section class="row">
+      <div class="col-md-2">
+        <sidebar/>
+      </div>
+      <div class="col-md-10">
+        <router-view/>
+      </div>
+    </section>
   </div>
 </template>
 
+<script>
+import sidebar from './components/geral/Sidebar.vue';
+
+export default {
+  components: {
+    sidebar,
+  },
+}
+</script>
+
 <style lang="scss">
+  #app {
+    .row {
+      margin-right: 0;
+    }
+  }
 </style>
