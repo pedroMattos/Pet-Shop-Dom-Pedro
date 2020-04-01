2779ca46e6e25779242ef773ab480073aadddec6
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-04-01 12:22:00 -0300

    add login page view

---
+++ pet-shop-dom-pedro/src/views/LoginPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <login-page/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import LoginPage from '@/components/LoginForm.vue';
+
+export default {
+  name: 'login',
+  components: {
+    'login-page': LoginPage,
+  },
+};
+</script>
