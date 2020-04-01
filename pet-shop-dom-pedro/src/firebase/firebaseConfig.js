9b1720771d9339618676fac82b5384c7e562059c
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-04-01 19:47:36 -0300

    add firebase config

---
+++ pet-shop-dom-pedro/src/firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCg1LaIIanZYPyQliFTmKYk5AD9rHan4BI",
+  authDomain: "pet-shop-dom-pedro-5e475.firebaseapp.com",
+  databaseURL: "https://pet-shop-dom-pedro-5e475.firebaseio.com",
+  projectId: "pet-shop-dom-pedro-5e475",
+  storageBucket: "pet-shop-dom-pedro-5e475.appspot.com",
+  messagingSenderId: "630386349015",
+  appId: "1:630386349015:web:bfc5e4b68bb314b7dd7192",
+  measurementId: "G-8EJ22P8V0T"
+};
---
+++ pet-shop-dom-pedro/src/firebase/firebaseInit.js
@@ -0,0 +1,8 @@
+import firebase from 'firebase';
+import 'firebase/firestore';
+import 'firebase/auth'
+import firebaseConfig from './firebaseConfig';
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+// exporta o metodo de autenticação e o banco de dados
+export default firebaseApp.auth() && firebaseApp.firestore();
