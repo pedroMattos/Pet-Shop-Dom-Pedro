d73e97b5abcb3fba82b71e925bcedac9d03115d0
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-04-01 12:21:10 -0300

    add new compoennt

---
+++ pet-shop-dom-pedro/src/components/LoginForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <form action="" id="form-login">
+    <input type="text" v-model="email" id="">
+    <input type="password" v-model="pass" id="">
+    <input type="submit" @click.prevent="login" value="Entrar">
+    <router-link class="link-sidebar" :to="{ name: 'Home' }">
+      Ir para a home sem login
+    </router-link>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'FormLogin',
+
+  data() {
+    return {
+      email: null,
+      pass: null,
+    };
+  },
+  methods: {
+    login() {
+      alert('Fazer logica de login');
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
