656ae3ba411c8f031adddfefc59058ee7bbc1771
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-04-01 20:05:40 -0300

    sidebar structure

---
+++ pet-shop-dom-pedro/src/components/geral/Sidebar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="sidebar">
+    <p>Sou a sidebar :P</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+};
+</script>
+
+<style lang="scss">
+  #sidebar {
+    height: 100vh;
+    background-color: #A0DFAA;
+    p {
+      margin: 0;
+    }
+  }
+</style>
