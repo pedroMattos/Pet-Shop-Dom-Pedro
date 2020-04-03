dce6b66b1bcaed74fbd12ea1f062373e165e0cee
antonioJRN <netinhorego13@gmail.com>
2020-04-03 16:38:15 -0300

    Div home e imagem adicionado

--- pet-shop-dom-pedro/src/views/LoginPage.vue
+++ pet-shop-dom-pedro/src/views/LoginPage.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <login-page/>
+  <div id="home" class="home">
+      <div id="overlay"></div>
+    <div id="form-div">
+      <login-page/>
+    </div>
   </div>
 </template>
 
