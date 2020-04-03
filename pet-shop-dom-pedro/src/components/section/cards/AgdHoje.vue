bd134655b11a2d37a362391d1326dd4ab2b11e00
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-04-03 18:17:09 -0300

    add new component

---
+++ pet-shop-dom-pedro/src/components/section/cards/AgdHoje.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="cards" id="adg-hoje">
+    <p>Hoje</p>
+    <p>Todos</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hoje-card',
+};
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ pet-shop-dom-pedro/src/components/section/cards/Estoque.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="cards" id="estoque">
+    <p>Estoque</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Estoque-card',
+};
+</script>
+
+<style lang="scss">
+
+</style>
