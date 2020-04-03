31e1911a2fa01f681ff1073178cbc48308279bec
antonioJRN <netinhorego13@gmail.com>
2020-04-03 16:34:24 -0300

    Adicionado router link

--- pet-shop-dom-pedro/src/components/LoginForm.vue
+++ pet-shop-dom-pedro/src/components/LoginForm.vue
@@ -1,14 +1,23 @@
 <template>
-  <form action="" id="form-login">
-    <input type="text" v-model="email" id="">
-    <input type="password" v-model="pass" id="">
-    <button class="btn btn-login" @click.prevent="login">
-      <span>Entrar</span>
-    </button>
-    <router-link class="link-sidebar" :to="{ name: 'Home' }">
-      Ir para a home sem login
-    </router-link>
-  </form>
+    <form action="" id="form-login">
+      <h1>Pet Shop Dom Pedro</h1>
+      <h1>login</h1>
+      <h3>E-mail:</h3>
+        <input type="text" v-model="email" id="email" >
+      <h3>Senha:</h3>
+        <input type="password" v-model="pass" id="password">
+      <button class="btn btn-login" @click.prevent="login">
+        <span>Entrar</span>
+      </button>
+      <!--<router-link class="link-sidebar" :to="{ name: 'Home' }">
+        Ir para a home sem login
+      </router-link>-->
+      <p>Não consegue logar? 
+        <router-link class="link-register" :to="{name: 'Register'}">
+          Entre aqui.
+        </router-link>
+      </p>
+    </form>
 </template>
 
 <script>
