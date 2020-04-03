ff1707306caceb8e0aad800d48b88f7bb2e130bf
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-04-03 18:16:38 -0300

    changes

--- pet-shop-dom-pedro/src/components/section/SectionMarketPlace.vue
+++ pet-shop-dom-pedro/src/components/section/SectionMarketPlace.vue
@@ -4,16 +4,21 @@
     <div class="col-md-6 col-sm-12">
       <sales-card></sales-card>
     </div>
+    <div class="col-md-6 col-sm-12">
+      <estoque-card></estoque-card>
+    </div>
   </section>
 </template>
 
 <script>
+import estoqueCard from './cards/Estoque.vue';
 import salesCard from './cards/Vendas.vue';
 
 export default {
   name: 'Sec-Markt',
   components: {
     'sales-card': salesCard,
+    'estoque-card': estoqueCard,
   },
 };
 </script>
