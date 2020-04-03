c257c8488c73cef231bad32b97210ba28d15d529
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-04-03 18:16:16 -0300

    import new component

--- pet-shop-dom-pedro/src/components/section/SectionAgendamentos.vue
+++ pet-shop-dom-pedro/src/components/section/SectionAgendamentos.vue
@@ -4,16 +4,21 @@
     <div class="col-md-6 col-sm-12">
       <news-agd></news-agd>
     </div>
+    <div class="col-md-6 col-sm-12">
+      <all-agd></all-agd>
+    </div>
   </section>
 </template>
 
 <script>
+import allAgd from './cards/AgdHoje.vue';
 import newsAgd from './cards/NovosAgendamentos.vue';
 
 export default {
   name: 'Section-agnd',
   components: {
     'news-agd': newsAgd,
+    'all-agd': allAgd,
   },
 };
 </script>
