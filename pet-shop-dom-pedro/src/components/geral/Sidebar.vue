31bf9dcb7b62ece32a977118c878be449ea630f0
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-04-03 22:59:22 -0300

    add list structure

--- pet-shop-dom-pedro/src/components/geral/Sidebar.vue
+++ pet-shop-dom-pedro/src/components/geral/Sidebar.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="sidebar">
-    <p>Sou a sidebar :P</p>
+    <p id="logo"><img src="../../image/pet.png" alt="Logo"></p>
+    <router-link :to="{ name: 'Home' }">
+      <p>Home</p>
+    </router-link>
+    <ul>
+      <router-link :to="{ name: 'MktPlace' }">
+        <li>Market Place</li>
+      </router-link>
+      <li>Agenda</li>
+      <li>Serviços</li>
+      <li>Feedback</li>
+    </ul>
     <logout/>
   </div>
 </template>
@@ -20,8 +31,24 @@ export default {
   #sidebar {
     height: 100vh;
     background-color: #A0DFAA;
+    ul {
+      list-style: none;
+      padding-inline-start: 0;
+      li {
+        margin: 5px 0;
+        font-size: 14px;
+      }
+    }
     p {
       margin: 0;
+    }
+    img {
+      margin-top: 20px;
+      width: 70px;
+      height: 70px;
+    }
+    #logo {
+      text-align: center;
     }
   }
 </style>
